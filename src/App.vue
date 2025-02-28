73f9f3c0d985140c7dc43d2af4aff4a47c2e2579
Renan Couto <renanmv10@hotmail.com>
2025-02-28 11:01:07 -0300

    Setting up an array of answers

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Our first vue js app"/>
+
+  <div>
+    <h1 v-html="this.question">
+    </h1>
+
+    <input type="radio" name="options" value="True">
+    <label>True</label><br>
+
+    <input type="radio" name="options" value="True">
+    <label>False</label><br>
+
+    <button class="send" type="button">Send</button>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  
+  data() {
+    return{
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswers: undefined,
+    }
+  },
+
+  computed: {
+    answers() {
+      var answers = [...this.incorrectAnswers];
+      answers.push(this.correctAnswers);
+      return answers;
+    }
+  },
+
+  created() {
+    this.axios
+    .get('https://opentdb.com/api.php?amount=1&category=18')
+    .then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswers = response.data.results[0].correct_answer;
+    })
   }
 }
+
+//https://opentdb.com/api.php?amount=1&category=18
 </script>
 
 <style lang="scss">
@@ -21,6 +57,24 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio] {
+    margin: 12px 4px;
+  }
+  
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+
 }
 </style>
